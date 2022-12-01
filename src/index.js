f4a71d25331470891719f8cfcc227c698b77d662
Дмитрий <dimas.savin1990@gmail.com>
2022-11-30 21:12:55 -0500

    hw 10 start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+};
+
+refs.input.addEventListener('input', debounce(onInputSubmit, DEBOUNCE_DELAY));
+
+function onInputSubmit() { };
 
 const DEBOUNCE_DELAY = 300;
