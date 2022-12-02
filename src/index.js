c6a8ad4eeafabca92c4a096d96453b7386205641
Дмитрий <dimas.savin1990@gmail.com>
2022-12-01 21:58:10 -0500

    add

--- src/index.js
+++ src/index.js
@@ -2,13 +2,45 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
 
+
+const DEBOUNCE_DELAY = 300;
+
+
 const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
+  countrysInfo: document.querySelector('.country-info'),
 };
 
 refs.input.addEventListener('input', debounce(onInputSubmit, DEBOUNCE_DELAY));
 
-function onInputSubmit() { };
+const clearList = () => (refs.countryList.innerHTML = '');
+const clearCountrys = () => (refs.countrysInfo.innerHTML = '');
 
-const DEBOUNCE_DELAY = 300;
+function onInputSubmit(e) {
+  let name = refs.input.ariaValueMax.trim();
+
+  if (name) {
+    fetchCountries(name)
+      .then(items => {
+        if (items.length > 10) {
+          Notify.info(
+            `Too many matches found. Please enter a more specific name.`,
+            { timeout: 1000 }
+          );
+        } else
+          items.length > 1 ? renderCountry(items) : renderSinglCountry(items);
+      })
+      .catch(onFechError);
+  }
+
+  clearList();
+  clearCountrys();
+}
+
+function onFechError() {
+  Notify.failure(`OOps, there is no country with that name`, { timeout: 1000 });
+};
+
+
+
